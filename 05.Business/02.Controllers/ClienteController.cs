bd5a1f4c1b9ee42cce3cd06a4360b5e2f336f9c5
tom-kay <bgfriend83@gmail.com>
2024-02-01 11:59:24 +0900

    iBatis.net 설치

---
+++ 05.Business/02.Controllers/ClienteController.cs
@@ -0,0 +1,28 @@
+﻿using _05.Business.Models.Dao;
+using _05.Business.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Business.Controllers
+{
+    class ClienteController
+    {
+        ClienteView Vista;
+        //Constructor
+        public ClienteController(ClienteView View)
+        {
+            Vista = View;
+            //inicializar eventos
+            Vista.Load += new EventHandler(ClientList);
+        }
+
+        private void ClientList(object sender, EventArgs e)
+        {
+            ClienteDao db = new ClienteDao();
+            //Vista.TablaCliente.DataSource = db.VerRegistros(Vista.txtBuscar.Text);
+        }
+    }
+}
