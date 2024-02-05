fe5f4106fb2aff4acb414374d928cad26dcb42ed
esjang67 <esj67@naver.com>
2024-02-05 21:51:55 +0900

    ibatis 연결테스트 완료

--- 05.Business/01.Models/Dao/CodeMntDao.cs
+++ 05.Business/01.Models/Dao/CodeMntDao.cs
@@ -9,27 +9,37 @@ using System.Threading.Tasks;
 
 namespace _05.Business._01.Models.Dao
 {
-	public class CodeMntDao
+	public class CodeMntDao : DaoFactory
 	{
-		
-		public IList<CodeMasterDto> GetCodeMasterList()
+
+    public static string FindDepartment(int deptId)
+    {
+
+      ISqlMapper mapper = EntityMapper;
+      string str = mapper.QueryForObject<string>("selectCodeMasterList", deptId);
+      return str;
+
+    }
+
+    public List<CodeMasterDto> GetCodeMasterList()
 		{
-			IList<CodeMasterDto> list = DaoFactory.getInstance.QueryForList<CodeMasterDto>("selectCodeMasterList", null);
+      ISqlMapper mapper = EntityMapper;
+      List<CodeMasterDto> list = mapper.QueryForList<CodeMasterDto>("selectCodeMasterList", null).ToList();
 			
 			return list;
 		}
 
-		public void insertEmployee(string emp)
-		{
-			DaoFactory.getInstance.Insert("insertEmployee", emp);
-		}
-		public void updateEmployee(string emp)
-		{
-			DaoFactory.getInstance.Update("updateEmployee", emp);
-		}
-		public void deleteEmployeeByEmpno(int id)
-		{
-			DaoFactory.getInstance.Delete("deleteEmployeeByEmpno", id);
-		}
+		//public void insertEmployee(string emp)
+		//{
+		//	DaoFactory.getInstance.Insert("insertEmployee", emp);
+		//}
+		//public void updateEmployee(string emp)
+		//{
+		//	DaoFactory.getInstance.Update("updateEmployee", emp);
+		//}
+		//public void deleteEmployeeByEmpno(int id)
+		//{
+		//	DaoFactory.getInstance.Delete("deleteEmployeeByEmpno", id);
+		//}
 	}
 }
--- 05.Business/01.Models/Dao/DaoFactory.cs
+++ 05.Business/01.Models/Dao/DaoFactory.cs
@@ -2,40 +2,29 @@
 using IBatisNet.DataMapper;
 using System.Xml;
 using IBatisNet.Common.Utilities;
+using System;
 
 
 namespace _05.Business._01.Models.Dao
 {
 	public class DaoFactory
 	{
-		private static object syncLock = new object(); 
-		private static ISqlMapper mapper = null; 
-		public static ISqlMapper getInstance
-		{
-			get
-			{
-				try
-				{
-					if (mapper == null)
-					{
-						lock (syncLock)
-						{
-							if (mapper == null)
-							{
-								DomSqlMapBuilder dom = new DomSqlMapBuilder();
-								XmlDocument sqlMapConf = Resources.GetEmbeddedResourceAsXmlDocument("_05.Business.SqlMap.config, SOODAL");
-								mapper = dom.Configure(sqlMapConf);
-							}
-						}
-					}
-				}
-				catch
-				{
-					throw;
-				}
-				return mapper;
-			}
+    public static ISqlMapper EntityMapper
+    {
+      get
+      {
+        try
+        {
+          ISqlMapper mapper = Mapper.Instance();
+          return mapper;
+        }
+        catch (Exception ex)
+        {
+          throw ex;
+        }
+      }
+    }
+  }
 
-		}
-	}
+  
 }
