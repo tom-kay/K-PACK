57481dbb7e3cdc5d1be5122c4dc685bdabfd0350
KAY <bgfriend83@gmail.com>
2024-11-25 22:21:48 +0900

    업그래이드 추가

---
+++ Launcher/Program.cs
@@ -0,0 +1,105 @@
+﻿using FluentFTP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public static class Program
+    {
+        public static string ftpHost = "kayroot.dothome.co.kr";
+        public static string ftpUser = "kayroot";
+        public static string ftpPass = "ftp#root24";
+        public static string ftpPath = "/html/soodal";
+        public static string localPath = Application.StartupPath; // 다운로드할 로컬 경로 설정
+        public static string executable = "soodal.exe";
+        public static string versionFilePath = ftpPath + "/version.txt";
+        public static string localVersionFilePath = Path.Combine(localPath, "version.txt");
+
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            if (CheckForUpdates())
+            {
+                using (Upgrade upgradeForm = new Upgrade())
+                {
+                    upgradeForm.Load += (sender, e) => { upgradeForm.StartDownload(); };
+                    Application.Run(upgradeForm);
+                }
+            }
+            else
+            {
+                RunExecutable();
+            }
+        }
+
+        public static bool CheckForUpdates()
+        {
+            // 서버에서 최신 버전 가져오기
+            string latestVersion = GetFtpFileVersion();
+
+            // 로컬 버전 가져오기
+            string currentVersion = File.Exists(localVersionFilePath) ? File.ReadAllText(localVersionFilePath).Trim() : "0.0.0.0";
+
+            // 버전이 같으면 업데이트 필요 없음
+            return string.Compare(latestVersion, currentVersion) > 0;
+        }
+
+        public static string GetFtpFileVersion()
+        {
+            string version = string.Empty;
+
+            using (var client = new FtpClient(ftpHost, ftpUser, ftpPass))
+            {
+                
+                client.Connect();
+                string localTempFilePath = Path.GetTempFileName();
+
+                client.DownloadFile(localTempFilePath, versionFilePath, FtpLocalExists.Overwrite, FtpVerify.None);
+
+                version = File.ReadAllText(localTempFilePath).Trim();
+                File.Delete(localTempFilePath);
+            }
+
+            return version;
+        }
+
+        public static void RunExecutable()
+        {
+            System.Diagnostics.Process.Start(Path.Combine(localPath, executable), "launcher");
+            Application.Exit();
+            //UpdateLauncher();
+        }
+
+        public static void UpdateLauncher()
+        {
+            string latestLauncherVersion = GetFtpFileVersion();
+
+            // 현재 런처의 버전 가져오기
+            string currentLauncherVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            if (string.Compare(latestLauncherVersion, currentLauncherVersion) > 0)
+            {
+                using (var client = new FtpClient(ftpHost, ftpUser, ftpPass))
+                {
+                    client.Connect();
+                    //DownloadFile(client, ftpPath + "/Launcher.exe", Path.Combine(localPath, "Launcher.exe"));
+                }
+                MessageBox.Show("런처가 업데이트되었습니다. 다시 시작해 주세요.", "업데이트 완료", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+        }
+
+    }
+}
---
+++ Launcher/Upgrade.cs
@@ -0,0 +1,141 @@
+﻿using FluentFTP.Helpers;
+using FluentFTP;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class Upgrade : Form
+    {
+        private static ProgressBar progressBar;
+        private static int totalFiles;
+        private static int downloadedFiles;
+        private static BackgroundWorker worker;
+
+        public Upgrade()
+        {
+            progressBar = new ProgressBar
+            {
+                Dock = DockStyle.Fill,
+                Minimum = 0,
+                Maximum = 100,
+                Value = 0
+            };
+
+            this.Controls.Add(progressBar);
+            this.Text = "파일 다운로드 진행 중...";
+            this.Size = new System.Drawing.Size(400, 100);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+            worker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_RunWorkerCompleted);
+            worker.WorkerReportsProgress = true;
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // 다운로드 작업 수행
+            DownloadFiles();
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //MessageBox.Show("파일 다운로드가 완료되었습니다.", "완료", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Program.RunExecutable(); // 다운로드 완료 후 실행
+        }
+
+        public void SetTotalFiles(int total)
+        {
+            totalFiles = total;
+            downloadedFiles = 0;
+        }
+
+        public void UpdateProgress()
+        {
+            downloadedFiles++;
+            int progressPercentage = (int)((double)downloadedFiles / totalFiles * 100);
+            worker.ReportProgress(progressPercentage);
+        }
+
+        public void StartDownload()
+        {
+            worker.RunWorkerAsync();
+        }
+
+        private void DownloadFiles()
+        {
+            using (var client = new FtpClient(Program.ftpHost, Program.ftpUser, Program.ftpPass))
+            {
+                client.Connect();
+                var items = client.GetListing(Program.ftpPath);
+
+                int fileCount = GetFileCount(client, Program.ftpPath);
+                SetTotalFiles(fileCount);
+
+                DownloadFileOrDirectory(client, Program.ftpPath, Program.localPath);
+
+                // 버전 파일 다운로드
+                client.DownloadFile(Program.localVersionFilePath, Program.versionFilePath, FtpLocalExists.Overwrite, FtpVerify.None);
+            }
+        }
+
+        private int GetFileCount(FtpClient client, string remotePath)
+        {
+            var items = client.GetListing(remotePath);
+            int count = 0;
+
+            foreach (var item in items)
+            {
+                if (item.Type == FtpObjectType.Directory)
+                {
+                    count += GetFileCount(client, item.FullName); // Recursively count files in subdirectories
+                }
+                else if (item.Type == FtpObjectType.File)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private void DownloadFileOrDirectory(FtpClient client, string remotePath, string localPath)
+        {
+            var items = client.GetListing(remotePath);
+
+            foreach (var item in items)
+            {
+                string localFilePath = Path.Combine(localPath, item.Name);
+
+                if (item.Type == FtpObjectType.Directory)
+                {
+                    if (!Directory.Exists(localFilePath))
+                    {
+                        Directory.CreateDirectory(localFilePath);
+                    }
+                    DownloadFileOrDirectory(client, item.FullName, localFilePath);
+                }
+                else if (item.Type == FtpObjectType.File)
+                {
+                    DownloadFile(client, item.FullName, localFilePath);
+                }
+            }
+        }
+
+        private void DownloadFile(FtpClient client, string remoteFile, string localFile)
+        {
+            client.DownloadFile(localFile, remoteFile, FtpLocalExists.Overwrite, FtpVerify.None);
+            UpdateProgress();
+        }
+    }
+}
